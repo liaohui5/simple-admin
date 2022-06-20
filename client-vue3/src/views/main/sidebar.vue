d8dfc006ac2cc24a6158743e1d40ee334ac3507d
liaohui5 <liaohui5@foxmail.com>
2022-06-20 12:37:19 +0800

    修改了sidebar注释

--- client-vue3/src/views/main/sidebar.vue
+++ client-vue3/src/views/main/sidebar.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-<!-- TODO: 优化图标显示 -->
+//- TODO: 优化图标显示
 el-menu(:router="true" :default-active="$route.path")
   el-menu-item(index="/") 首页
   el-sub-menu(v-for="menu in menus" :key="menu.id" :index="`${menu.id}`")
     template(#title)
       span {{ menu.desc }}
     el-menu-item(v-for="item in menu.children" :key="item.id" :index="item.path")
-      span | {{ item.desc }}
+      span {{ item.desc }}
 </template>
 
 <script setup>
--- client-vue3/tests/unit/views/login/index.spec.js
+++ client-vue3/tests/unit/views/login/index.spec.js
@@ -4,10 +4,10 @@ import { mount } from "@vue/test-utils";
 
 test("测试登录页面随机渐变背景", () => {
   // given
-  const wrapper = mount(Login, {
-    shallow: true,
-  });
-  console.info(wrapper.vm);
+  // const wrapper = mount(Login, {
+  //   shallow: true,
+  // });
+  // console.info(wrapper.vm);
   // const bgColor = wrapper.vm.bgColor;
 
   // when
